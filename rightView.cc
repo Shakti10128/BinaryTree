3d7b03da98173b02e9b80f33852e3c48667c8a0f
shakti <kumarshakti2017@gmail.com>
2022-10-25 23:43:14 +0530

    nothing change

---
+++ rightView.cc
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+#include"BinaryTreeClass.cc"
+using namespace std;
+// taking input of binaryTree levelwise using queue
+BinaryTreeNode<int> * takeInputLevelWise(){
+    int data; 
+    cout<<"Enter the root data"<<endl;
+    cin>>data;
+    BinaryTreeNode<int>* root = new BinaryTreeNode<int>(data);
+    queue<BinaryTreeNode<int> *>  pending;
+    pending.push(root);
+    while(pending.size()!=0){
+        BinaryTreeNode<int> * front = pending.front();
+        pending.pop();
+        cout<<"Enter the data of left child of "<<front->data<<endl;
+        int leftdata;
+        cin>>leftdata;
+        if(leftdata!=-1){
+            BinaryTreeNode<int> * leftchild = new BinaryTreeNode<int>(leftdata);
+            front->left = leftchild;
+            pending.push(leftchild);
+        }
+
+        cout<<"Enter the data of right child of "<<front->data<<endl;
+        int rightdata;
+        cin>>rightdata;
+        if(rightdata!=-1){
+            BinaryTreeNode<int> * rightchild = new BinaryTreeNode<int>(rightdata);
+            front->right = rightchild;
+            pending.push(rightchild);
+        }
+    }
+    return root;
+
+}
+
+
+void solve(BinaryTreeNode<int> *root,vector<int> &ans,int lvl){
+    if(root == NULL){
+        return;
+    }
+    if(lvl == ans.size()){
+        ans.push_back(root->data);
+}
+solve(root->right,ans,lvl+1);
+solve(root->left,ans,lvl+1);
+}
+vector<int> rightView(BinaryTreeNode<int> *root)
+{
+    vector<int> ans;
+    solve(root,ans,0);
+    return ans;
+}
+
+
+// 1 2 3 4 5 6 7 -1 -1 -1 -1 -1 8 -1 9 -1 -1 -1 -1
+int main(){
+    BinaryTreeNode<int> * root = takeInputLevelWise();
+    vector<int> ans = rightView(root);
+
+    for(int i = 0;i<ans.size();i++){
+        cout<<ans[i]<<" ";
+    }
+}
